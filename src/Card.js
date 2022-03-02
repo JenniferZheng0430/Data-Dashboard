7c75a97b4761e8821a9e226efd12b660930f27c6
Jennifer Zheng <jinqingmei@JenniferdeMacBook.local>
2022-03-02 14:14:23 +0400

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+function Card(){
+    return (
+        <>
+        <div className="header">
+            <h1>Project 1</h1>
+        </div>
+        <div className="header-researcher">
+            <h2>Researcher Name</h2>
+        </div>
+        <img src="img/Al Hijra.jpeg"></img>
+        <button className="btn">Detail</button>
+        </>
+    );
+}
+
+export default Card;
